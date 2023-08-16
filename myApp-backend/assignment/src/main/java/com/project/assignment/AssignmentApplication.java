2b87de66afb8274300ced0849cafbbcac1cc70e1
Rajesh Kalita <rajeshlk18@gmail.com>
2023-08-16 14:38:15 +0530

    Setup Initial API Requests for backend

--- myApp-backend/assignment/src/main/java/com/project/assignment/AssignmentApplication.java
+++ myApp-backend/assignment/src/main/java/com/project/assignment/AssignmentApplication.java
@@ -2,8 +2,12 @@ package com.project.assignment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.project.assignment.repository")
+@EntityScan(basePackages = "com.project.assignment.entity")
 public class AssignmentApplication {
 
 	public static void main(String[] args) {
